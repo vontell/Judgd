c1ee28444941ab67c8cf89517e2524255c2f2bc9
Aaron Vontell <vontell@mit.edu>
2016-09-24 03:22:11 -0400

    Add logger and new functions

--- scraper.py
+++ scraper.py
@@ -2,10 +2,12 @@ from urllib.request import urlopen
 from pymongo import MongoClient
 import requests
 import operator
+import logging
 
 # Some useful base constants (for URLS and such)
 project_listings = "http://devpost.com/software/search?page="
 database = "localhost:27017"
+logging.basicConfig(filename='devpost.log',level=logging.DEBUG)
 
 # The database instance
 client = MongoClient(database)
@@ -51,7 +53,8 @@ def get_hackathons():
         # Increment the page number
         page = page + 1
     
-def get_top_tech():
+# Returns the most popular tags for winning
+def get_top_tags():
     winners = db.devpost.find({"winner": True})
     tech = {}
     for winner in winners:
@@ -64,5 +67,27 @@ def get_top_tech():
                 
     return sorted(tech.items(), key=operator.itemgetter(1))
 
-print(get_top_tech())
+# Returns the most popular tags for winning, 
+# minus programming languages, from the given list
+def remove_languages(tag_list):
+    languages = ["javascript", "android", "java", "css", "html", "html5", "jquery", "swift", "python", "css3", "c#"]
+    return [tag for tag in tag_list if tag[0] not in languages]
+    
+# Returns the most popular technology of 
+# losers
+def get_worst_tech():
+    losers = db.devpost.find({"winner": False})
+    tech = {}
+    for loser in losers:
+        if loser.get("tags"):
+            for tag in loser.get("tags"):
+                if tag in tech:
+                    tech[tag] = tech[tag] + 1
+                else:
+                    tech[tag] = 1
+                
+    return sorted(tech.items(), key=operator.itemgetter(1))
+
+#print(get_top_tech())
+logging.info(remove_languages(get_worst_tech()))
 #get_hackathons()
